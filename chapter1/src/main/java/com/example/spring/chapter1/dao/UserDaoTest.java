9764e255e3fa3d08e5337b27543166ace423e66c
BJH <imganchi@gmail.com>
2025-03-21 17:52:37 +0900

    fix

--- chapter1/src/main/java/com/example/spring/chapter1/dao/UserDaoTest.java
+++ chapter1/src/main/java/com/example/spring/chapter1/dao/UserDaoTest.java
@@ -1,11 +1,13 @@
 package com.example.spring.chapter1.dao;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-
-        UserDao userDao = new UserDao(connectionMaker);
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
     }
 }
